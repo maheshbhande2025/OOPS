c05be1c6394df776026ac2178ee1c18d2da33f24
maheshbhande2025 <maheshbhande2025.etc@mmcoe.edu.in>
2026-09-08 04:04:15 +0000

    t

--- operator_overloding.cpp
+++ operator_overloding.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 class complex
 {
-    int r, i;
+    double r, i;
 
 public:
-    void read(int real, int img)
+    void read(double real, double img)
     {
         r = real;
         i = img;
@@ -33,20 +33,53 @@ public:
 
         return res;
     }
+    complex operator-(complex b)
+    {
+        complex res;
+
+        res.r = r - b.r;
+        res.i = i - b.i;
+
+        return res;
+    }
+    complex operator*(complex b)
+    {
+        complex res;
+        res.r = (r * b.r) - (i * b.i);
+        res.i = (i * b.r) + (r * b.i);
+        return res;
+    }
+    complex operator/(complex b)
+    {
+        complex res;
+        double dino = (b.r * b.r) + (b.i * b.i);
+        res.r = ((r * b.r) + (i * b.i)) / dino;
+        res.i = ((i * b.r) - (r * b.i)) / dino;
+
+        return res;
+    }
 };
-int main(){
-    int r1,i1,r2,i2;
-    complex c1,c2,c3;
-    cout<<"Enter the real and imaginary parts of first complex no:"<<endl;
-    cin>>r1>>i1;
-    
-    cout<<"Enter the real and imaginary parts of second complex no:"<<endl;
-    cin>>r2>>i2;
-    c1.read(r1,i1);
-    c2.read(r2,i2);
-    cout<<"The sum is";
-    c3=c1+c2;
+int main()
+{
+    int r1, i1, r2, i2;
+    complex c1, c2, c3;
+    cout << "Enter the real and imaginary parts of first complex no:" << endl;
+    cin >> r1 >> i1;
+
+    cout << "Enter the real and imaginary parts of second complex no:" << endl;
+    cin >> r2 >> i2;
+    c1.read(r1, i1);
+    c2.read(r2, i2);
+    cout << "The sum is: ";
+    c3 = c1 + c2;
     c3.display();
-
-
+    cout << "\nThe substraction is: ";
+    c3 = c1 - c2;
+    c3.display();
+    cout << "\nThe multiplication is: ";
+    c3 = c1 * c2;
+    c3.display();
+    cout << "\nThe Division is: ";
+    c3 = c1 / c2;
+    c3.display();
 }
